82d78b6d512f14e44c2c9562fe739fa267e906e3
Gaetz <gaetz@rufflerim.com>
2017-11-05 13:03:22 +0100

    Successful api request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '04780c572270ed7c541b98c0c1edc318';
-const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
