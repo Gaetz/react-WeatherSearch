0dffb0f3d837f4e276cc6c1a22ff273e58dadf0d
Gaetz <gaetz@rufflerim.com>
2017-11-05 20:13:58 +0100

    Improve charts

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -6,16 +6,16 @@ class WeatherList extends Component {
 
     renderWeather(cityData) {
         const name = cityData.city.name;
-        const temperatures = cityData.list.map(weather => weather.main.temp);
+        const temperatures = _.map(cityData.list.map(weather => weather.main.temp), temp => temp - 273 ); // Convert to °C
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
         
         return(
             <tr key={name}>
                 <td>{name}</td>
-                <td><Chart data={temperatures} color='orange' /></td>
-                <td><Chart data={pressures} color='green' /></td>
-                <td><Chart data={humidities} color='blue' /></td>
+                <td><Chart data={temperatures} color='orange' units='°C' /></td>
+                <td><Chart data={pressures} color='green' units='hPa' /></td>
+                <td><Chart data={humidities} color='blue' units='%' /></td>
             </tr>
         );
     }
