4b0b6e87baf867ef95376297a40190e72621071e
Gaetz <gaetz@rufflerim.com>
2017-11-05 12:04:27 +0100

    Prevent form to POST submit

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -5,7 +5,8 @@ export default class SearchBar extends Component {
     constructor(props) {
         super(props);
         this.onInputChange = this.onInputChange.bind(this);
-
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+        
         this.state = { term: '' };        
     }
 
@@ -13,9 +14,13 @@ export default class SearchBar extends Component {
         this.setState({ term: event.target.value }); 
     }
 
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form className='input-group'>
+            <form className='input-group' onSubmit={this.onFormSubmit}>
                 <input placeholder='Type your city' className='form-control' value={this.state.term} onChange={this.onInputChange} />
                 <span className='input-group-btn'>
                     <button type='submit' className='btn btn-secondary'>Search</button>
